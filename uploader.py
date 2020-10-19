7da9be1258c27a306d1d82cd24e9ec73246f5e7c
Pieter van der Kooi <piko@tulexi.net>
2020-10-20 00:10:15 +0200

    youtube-dl
    Added in YoutubeDL support so you can import files from online platforms. In the current form it first downloads the files and then uploads them using ffmpeg.

--- uploader.py
+++ uploader.py
@@ -1,5 +1,7 @@
 from mttkinter import mtTkinter as tk
 import tkinter.filedialog as filedialog
+import youtube_dl
+import re
 import subprocess
 import threading
 import psutil, os
@@ -8,6 +10,7 @@ import configparser
 
 root = tk.Tk()
 
+videoPath = "./Temp_Imported_vid.mp4"
 #Convert to mttkinter
 Label = tk.Label
 Entry = tk.Entry
@@ -26,9 +29,32 @@ streamkey = ''
 if config.has_option('ATHENA', 'streamkey'):
     streamkey = tk.StringVar(root, value=config['ATHENA']['Streamkey'])
 
-
+#Check if old video exists and if so deletes it.
+if os.path.exists(videoPath):
+    os.remove(videoPath)
 
 def ffmpeg(inp, out):
+    class MyLogger(object):
+        def debug(self, msg):
+            print("DEBUG " + msg)
+            pass
+
+        def warning(self, msg):
+            print("WARNING " + msg)
+            pass
+
+        def error(self, msg):
+            print("ERROR " + msg)
+
+    def progress_hook(response):
+        if response["status"] == "downloading":
+            textProgress.delete(1.0, END)
+            textProgress.insert(END, "Downloading: "+str(round(response["downloaded_bytes"]*100/response["total_bytes"],2))+"%")
+        if response["status"] == "finished":
+            uploadLocal(videoPath, out)
+        if response["status"] == "error":
+            textProgress.delete(1.0, END)
+            textProgress.insert(END, "Error occured while downloading video.")
     def cleanExit(pid, including_parent=True):
         parent = psutil.Process(pid)
         children = parent.children(recursive=True)
@@ -39,15 +65,36 @@ def ffmpeg(inp, out):
             parent.kill()
             parent.wait(5)
 
-    cmd = "ffmpeg -i "+inp+" "+out
-    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,universal_newlines=True)
-    buttonExit.config(command=lambda: cleanExit(process.pid))
-    
-    while process.poll() is None:
-        for line in process.stdout:
-            textProgress.delete(1.0, END)
-            textProgress.insert(END, line)
-    
+    def uploadLocal(inp, out):
+            cmd = "ffmpeg -i "+inp+" -codec copy -f flv "+out
+            process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,universal_newlines=True)
+            buttonExit.config(command=lambda: cleanExit(process.pid))
+            
+            while process.poll() is None:
+                for line in process.stdout:
+                    textProgress.delete(1.0, END)
+                    textProgress.insert(END, line)
+
+    def uploadWeb(inp, out):
+            textProgress.insert(END, 'Downloading video from: ' + inp)
+            ydl_opts = {
+                'outtmpl': videoPath,
+                'format': 'best',
+                'logger': MyLogger(),
+                "progress_hooks": [progress_hook],
+            }
+
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([inp])
+
+    #Check if input is a valid url, if not assume it is a local file.
+    if re.match(isURL, inp) is not None:
+        print('Trying to upload from URL.')
+        uploadWeb(inp, out)
+    else:
+        print('Not an URL, trying to upload local file.')
+        uploadLocal(inp, out)
+
     #FFMpeg finished running. 
     buttonExit.config(command=sys.exit)
     textProgress.delete(1.0, END)
@@ -75,6 +122,14 @@ def startUpload():
     fProcess = ffmpeg(fileInput, streamOutput)
 
 t = threading.Thread(target=startUpload)
+
+isURL = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
 #Creating labels and corresponding input fields.
 labelWarning = Label(root, text="Make sure to use the exit button to close this program to make sure all processes end and nothing continues running in the background.")
